9d43b64f47601d4b78cb9051bcb16acb5ac42ec0
waizeshaikh123 <140107406+waizeshaikh123@users.noreply.github.com>
2024-04-15 13:01:58 +0500

    fb=-be

--- fb/views.py
+++ fb/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render,HttpResponse
-
+from fb.models import Contact
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
     return render(request, "index.html")
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        password  = request.POST['name']
+        sv = Contact(name=name, email=email, password=password)
+        sv.save()
+        messages.success(request, "Your message has been sent")
+    return render(request, 'contact.html')
